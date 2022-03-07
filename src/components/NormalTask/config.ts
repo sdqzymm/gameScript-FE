56ec89071f15ff510505efbd446bf2556efff655
522532683@qq.com <522532683@qq.com>
2022-03-07 16:53:03 +0800

    fix(normaltask): 修复时间的获取逻辑

--- src/components/NormalTask/config.ts
+++ src/components/NormalTask/config.ts
@@ -237,7 +237,7 @@ options[1].children = options[1].children.filter((item) => {
   if (day === 0) day = 7
   if (hour < 12) day--
   if (day === 7) return true
-  if (mapElemental[day].includes(item.value)) return true
+  if (mapElemental[day - 1].includes(item.value)) return true
   return false
 })
 
