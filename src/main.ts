2381ae3a0d2550e8afaef4446e64e833ddcf3531
522532683@qq.com <522532683@qq.com>
2022-03-08 11:59:25 +0800

    feat(main.ts): 引入messagebox, loaing组件的样式

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import store from './store'
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
 import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
+import 'element-plus/es/components/loading/style/css'
 
 const app = createApp(App)
 
