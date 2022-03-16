57942e4d37864f8abfa0175559f3810fccccec9c
522532683@qq.com <522532683@qq.com>
2022-03-16 15:44:13 +0800

    fix(配置任务-活动): 活动引起递归

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -4,11 +4,7 @@ export default () => {
   const store = useStore()
   const config = computed(() => {
     const config = store.state.config
-    // 移除活动
-    config.tasks = config.tasks.filter((task) => {
-      if (task.type === '活动') return false
-      return true
-    })
+
     return config
   })
   return config
