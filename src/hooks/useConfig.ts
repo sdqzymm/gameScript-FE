c13afcd35f7beae5499732f9507d4643a1512b34
522532683@qq.com <522532683@qq.com>
2022-03-16 15:22:32 +0800

    fix(配置任务): 移除活动类型

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -3,7 +3,13 @@ import { useStore } from '../store'
 export default () => {
   const store = useStore()
   const config = computed(() => {
-    return store.state.config
+    const config = store.state.config
+    // 移除活动
+    config.tasks = config.tasks.filter((task) => {
+      if (task.type === '活动') return false
+      return true
+    })
+    return config
   })
   return config
 }
